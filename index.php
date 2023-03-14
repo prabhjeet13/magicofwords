73b960d036b18073dd3f3607674894a3a7d64514
iamrajambm6 <iamrajaraj11@gmail.com>
2023-03-14 23:09:45 +0530

    index.php added

--- index.php
+++ index.php
@@ -1,2 +1,194 @@
-<?php 
-    echo "hello!!!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Explore Blogs</title>
+    
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/signin.html">
+
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <nav>
+        <div class="container nav_container">
+            <a href="index.html" class="nav_logo">MAGIC OF WORDS</a>
+            <ul class="nav_items">
+                <li> <a href="blog.html">Explore</a></li>
+                <li> <a href="/signin.html">Sign In</a></li>
+                <li> <a href="about.html">About Us</a></li>
+                <li> <a href="contact.html">Contact Us</a></li>
+                <li class="nav_profile">
+
+                    <div class="mw">
+                        <a href="#" class="btn btn-primary">MW</a>
+                    </div>
+                    <ul>
+                        <li><a href="dashboard.html">Dashboard</a></li>
+                        <li><a href="logout.html">Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <button id="open_nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close_nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
+    <!-- end nav -->
+
+    <section class="featured">
+        <div class="container featured_container">
+            <div class="post_thumbnail">
+                <img src="/images/wildlife.jpg">
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Wild Life</a>
+                <h2 class="post_title"> <a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum necessitatibus adipisci maiores debitis ut, laborum, culpa perspiciatis dolore praesentium quo unde amet eligendi, eius vero qui? Excepturi, repudiandae. Nemo, odio?</a> </h2>
+                <p class="post_body">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur illo excepturi adipisci maxime distinctio ut iste at nulla. Omnis ipsum voluptatem, magni neque obcaecati explicabo consequatur sapiente vero expedita! Voluptatem.
+                </p>
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Raja Raj</h5>
+                        <small>June 10,2021 - 07-30</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- --------------------------------------- -->
+
+<section class="post">
+    <div class="container post_container">
+        <article class="post">
+            <div class="post_thumbnail">
+                <img src="/images/nature1.jpg" >
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Flower</a>
+                <h3 class="post_title">
+                    <a href="post.html"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga inventore ratione, dignissimos odit, nihil possimus molestiae at aliquid rerum enim distinctio labore quo dicta? </a>
+                </h3>
+                <p class="post_body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident unde exercitationem nemo deleniti possimus repellat quibusdam at ullam maxime, quae iure doloremque illo? </p>
+                
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Laxmi</h5>
+                        <small>june 13,2032 - 08-40</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post_thumbnail">
+                <img src="/images/nature.jpg" >
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Nature</a>
+                <h3 class="post_title">
+                    <a href="post.html"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga inventore ratione, dignissimos odit, nihil possimus molestiae at aliquid rerum enim distinctio labore quo dicta? </a>
+                </h3>
+                <p class="post_body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident unde exercitationem nemo deleniti possimus repellat quibusdam at ullam maxime, quae iure doloremque illo? </p>
+                
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Laxmi</h5>
+                        <small>june 13,2032 - 08-40</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+        
+        <article class="post">
+            <div class="post_thumbnail">
+                <img src="/images/fountain.jpg" >
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Fountain</a>
+                <h3 class="post_title">
+                    <a href="post.html"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga inventore ratione, dignissimos odit, nihil possimus molestiae at aliquid rerum enim distinctio labore quo dicta? </a>
+                </h3>
+                <p class="post_body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident unde exercitationem nemo deleniti possimus repellat quibusdam at ullam maxime, quae iure doloremque illo? </p>
+                
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Laxmi</h5>
+                        <small>june 13,2032 - 08-40</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+        
+        <article class="post">
+            <div class="post_thumbnail">
+                <img src="/images/nature.jpg" >
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Nature</a>
+                <h3 class="post_title">
+                    <a href="post.html"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga inventore ratione, dignissimos odit, nihil possimus molestiae at aliquid rerum enim distinctio labore quo dicta? </a>
+                </h3>
+                <p class="post_body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident unde exercitationem nemo deleniti possimus repellat quibusdam at ullam maxime, quae iure doloremque illo? </p>
+                
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Laxmi</h5>
+                        <small>March 13,2032 - 08-40</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+        
+        <article class="post">
+            <div class="post_thumbnail">
+                <img src="/images/animal.jpg" >
+            </div>
+            <div class="post_info">
+                <a href="" class="category_button">Animal</a>
+                <h3 class="post_title">
+                    <a href="post.html"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga inventore ratione, dignissimos odit, nihil possimus molestiae at aliquid rerum enim distinctio labore quo dicta? </a>
+                </h3>
+                <p class="post_body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident unde exercitationem nemo deleniti possimus repellat quibusdam at ullam maxime, quae iure doloremque illo? </p>
+                
+                <div class="post_author">
+                    <div class="post_author-info">
+                        <h5>By:Prabhjeet</h5>
+                        <small>November 18,2032 - 12:40</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+    </div>
+</section>
+
+<!-- end post -->
+
+    <section class="category_button">
+        <div class="container category_button-container">
+            <a href="" class="category_button">Art</a>
+            <a href="" class="category_button">Nature</a>
+            <a href="" class="category_button">Animal</a>
+            <a href="" class="category_button">Journal</a>
+            <a href="" class="category_button">Travel</a>
+            <a href="" class="category_button">Science & Technology</a>
+        </div>                         
+    </section>
+
+
+    <!-- end category -->
+
+    <footer>
+        <div class="footer_copyright">
+            <small>Copyright &copy; WORDS OF MAGIC</small>
+        </div>
+    </footer>
+
+</body>
+</html>
