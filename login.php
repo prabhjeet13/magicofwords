3906cd9a7dea8b0b5320368b036a342745e9bb9e
laxmi <laxminarayan.2022ca046@mnnit.ac.in>
2023-03-14 23:31:23 +0530

    login.php and register.php added

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Explore Blogs</title>
+    <link rel="stylesheet" href="/css/style.css">
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <section class="form_section-conainer">
+        <div class="container form_section-conainer">
+            <h2>Sign In</h2>
+            <!-- <div class="alert_message success">
+                <p>This is an success message</p>
+            </div> -->
+                    <form action="">
+                <input type="text" placeholder="UserName or email">
+                <!-- <input type="email" placeholder="Email"> -->
+                <input type="password" placeholder="Password">
+                <div class="form_control">
+                    <!-- <label for="avatar">User Image</label>
+                    <input type="file" if="avatar"> -->
+                     </div>
+                     <button type="submit" class="signupbtn">Log In</button>
+                     <small>Don't have account? <a href="Signup.html">Register</a></small>
+                    </form>
+
+        </div>
+    </section>
+</body>
+</html>
+
+   
---
+++ register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Explore Blogs</title>
+    <link rel="stylesheet" href="/css/style.css">
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <section class="form_section-conainer">
+        <div class="container form_section-conainer">
+            <h2>Register Yourself!</h2>
+            <!-- <div class="alert_message error">
+                <p>This is an error message</p>
+            </div> -->
+                    <form action=""enctype="multipart/form-data">
+                <input type="text" placeholder="First Name" required>
+                <input type="text" placeholder="Last Name" required>
+                <input type="text" placeholder="UserName" required>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Create password">
+                <input type="password" placeholder="Conform Password">
+                <div class="form_control">
+                    <!-- <label for="avatar">User Image</label>
+                    <input type="file" if="avatar"> -->
+                     </div>
+                     <button type="submit" class="signupbtn">Sign Up</button>
+                     <small>Already have an account? <a href="Signin.html">Sign In</a></small>
+                    </form>
+
+        </div>
+    </section>
+</body>
+</html>
+
+   
